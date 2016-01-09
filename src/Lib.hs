0bd872770108f9e3d66e76a6a754d250e75c7308
Stephen O'Brien <wayofthepie@gmail.com>
2016-01-09 11:18:38 +0000

    Hacking about with the API

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,69 @@
 {-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE RankNTypes       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
 module Lib
     ( startApp
     ) where
 
+import Control.Monad.Trans.Either
+import Control.Monad.Reader
 import Data.Aeson
 import Data.Aeson.TH
+import Data.ByteString.Char8 as BS
+import qualified Data.Text as T
+import Data.Proxy
+import Database.Neo4j as Neo
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+import Debug.Trace
 
-$(deriveJSON defaultOptions ''User)
+data Component = Component
+    { summary :: T.Text
+    , product :: T.Text -- in cpe form...?
+    , cvssScore:: T.Text
+    } deriving (Eq, Show)
 
-type API = "users" :> Get '[JSON] [User]
+$(deriveJSON defaultOptions ''Component)
+
+type ReaderAPI = "components" :> ReqBody '[JSON] [Component] :> Post '[JSON] [Component]
 
 startApp :: IO ()
 startApp = run 8080 app
 
 app :: Application
-app = serve api server
+app = serve readerAPI readerServer
 
-api :: Proxy API
+api :: Proxy ReaderAPI
 api = Proxy
 
-server :: Server API
-server = return users
+readerServerT :: ServerT ReaderAPI (Reader String)
+readerServerT = postComponents
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+readerServer :: Server ReaderAPI
+readerServer = enter readerToEither readerServerT
+
+postComponents :: [Component] -> Reader String [Component]
+postComponents cs = do
+    s <- ask
+    return (traceShow s $ cs)
+
+readerToEither :: Reader String :~> EitherT ServantErr IO
+readerToEither = Nat readerToEither'
+
+readerToEither' :: forall a. Reader String a -> EitherT ServantErr IO a
+readerToEither' r = return (runReader r "hi")
+
+readerAPI :: Proxy ReaderAPI
+readerAPI = Proxy
+
+
+{-
+-- Custom monad for this server
+connReaderToEither' :: forall a. Neo.Hostname -> Neo.Port -> Reader Neo.Connection a -> EitherT ServantErr IO a
+connReaderToEither' host port r = do
+    conn <- newAuthConnection host port ("tets","test")
+    return (lift $ runReader r conn)
+-}
